214f6535ed45dfa080755fb337174bab34455899
RaxoTheOne <traxon.bg@gmail.com>
2024-01-03 15:54:37 +0100

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -21,27 +21,31 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int crossAxisCount = _calculateCrossAxisCount(context);
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('GridView'),
+        title: const Text('Non-Responsive GridView'),
       ),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: crossAxisCount,
-          crossAxisSpacing: 8.0,
-          mainAxisSpacing: 8.0,
-        ),
-        itemCount: 60, // Anzahl der Container
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            color: _getRandomColor(),
-            height: 100,
-            width: 150,
-            child: Center(
-              child: Text('Element $index'),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          int crossAxisCount = _calculateCrossAxisCount(constraints.maxWidth);
+
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: crossAxisCount,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
             ),
+            itemCount: 10, // Anzahl der Container
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                color: _getRandomColor(),
+                height: 100,
+                width: 150,
+                child: Center(
+                  child: Text('Element $index'),
+                ),
+              );
+            },
           );
         },
       ),
@@ -69,11 +73,13 @@ class MyHomePage extends StatelessWidget {
   }
 
   // Funktion zum Berechnen der Anzahl der Elemente pro Reihe basierend auf der Bildschirmbreite
-  int _calculateCrossAxisCount(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-
+  int _calculateCrossAxisCount(double screenWidth) {
     // Je nach Bildschirmbreite unterschiedliche Anzahl an Elementen pro Reihe
-    if (screenWidth > 600) {
+    if (screenWidth > 1200) {
+      return 6;
+    } else if (screenWidth > 900) {
+      return 5;
+    } else if (screenWidth > 600) {
       return 4;
     } else if (screenWidth > 400) {
       return 3;
